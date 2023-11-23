ce2e7e81bdc1f0b138de53cf79a9a4a8319b64ac
sansangela <sansangela@hotmail.com>
2023-11-22 22:12:47 -0500

    Fix: update single-broadcast kernel

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include <immintrin.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <x86intrin.h>
 #include "graph_reader_csr.c"
 
@@ -24,107 +25,145 @@ static __inline__ unsigned long long rdtsc(void) {
   return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
 }
 
-#define kernel_add2(r0, r1) r0 = _mm256_add_epi32(r0, r1);
 
-#define kernel_add4(r0, r1, r2, r3)                                            \
-  kernel_add2(r0, r1) kernel_add2(r2, r3) r0 = _mm256_add_epi32(r0, r2);
+#define kernel1(A_0_columns, a1_broadcast, r0, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  flag = !_mm256_testz_si256(r0, r0);
 
-#define kernel_add8(r0, r1, r2, r3, r4, r5, r6, r7)                            \
-  kernel_add4(r0, r1, r2, r3) kernel_add4(r4, r5, r6, r7) r0 =                 \
-      _mm256_add_epi32(r0, r4);
+#define kernel2(A_0_columns, a1_broadcast, r0, r1, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast); \
+  flag = (!_mm256_testz_si256(r0, r0)) | \
+        ((!_mm256_testz_si256(r1, r1)) << 1);
 
-#define kernel_add16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12,    \
-                     r13, r14, r15)                                            \
-  kernel_add8(r0, r1, r2, r3, r4, r5, r6, r7)                                  \
-      kernel_add8(r8, r9, r10, r11, r12, r13, r14, r15) r0 =                   \
-          _mm256_add_epi32(r0, r8);
+#define kernel4(A_0_columns, a1_broadcast, r0, r1, r2, r3, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r2 = _mm256_loadu_si256(&A_0_columns[j+16]); \
+  r3 = _mm256_loadu_si256(&A_0_columns[j+24]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast); \
+  r2 = _mm256_cmpeq_epi32(r2, a1_broadcast); \
+  r3 = _mm256_cmpeq_epi32(r3, a1_broadcast); \
+  flag = (!_mm256_testz_si256(r0, r0)) | \
+        ((!_mm256_testz_si256(r1, r1)) << 1) | \
+        ((!_mm256_testz_si256(r2, r2)) << 2) | \
+        ((!_mm256_testz_si256(r3, r3)) << 3);
 
-#define kernel1(A_0_columns, a1_broadcast, r0, j, flag)                        \
-  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast);                                   \
-  flag |= !_mm256_testz_si256(r0, r0);
+#define kernel4(A_0_columns, a1_broadcast, r0, r1, r2, r3, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r2 = _mm256_loadu_si256(&A_0_columns[j+16]); \
+  r3 = _mm256_loadu_si256(&A_0_columns[j+24]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast); \
+  r2 = _mm256_cmpeq_epi32(r2, a1_broadcast); \
+  r3 = _mm256_cmpeq_epi32(r3, a1_broadcast); \
+  flag = (!_mm256_testz_si256(r0, r0)) | \
+        ((!_mm256_testz_si256(r1, r1)) << 1) | \
+        ((!_mm256_testz_si256(r2, r2)) << 2) | \
+        ((!_mm256_testz_si256(r3, r3)) << 3);
 
-#define kernel2(A_0_columns, a1_broadcast, r0, r1, j, flag)                    \
-  kernel1(A_0_columns, a1_broadcast, r0, j, flag)                              \
-      kernel1(A_0_columns, a1_broadcast, r1, j, flag)
+#define kernel8(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r2 = _mm256_loadu_si256(&A_0_columns[j+16]); \
+  r3 = _mm256_loadu_si256(&A_0_columns[j+24]); \
+  r4 = _mm256_loadu_si256(&A_0_columns[j+32]); \
+  r5 = _mm256_loadu_si256(&A_0_columns[j+40]); \
+  r6 = _mm256_loadu_si256(&A_0_columns[j+48]); \
+  r7 = _mm256_loadu_si256(&A_0_columns[j+56]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast); \
+  r2 = _mm256_cmpeq_epi32(r2, a1_broadcast); \
+  r3 = _mm256_cmpeq_epi32(r3, a1_broadcast); \
+  r4 = _mm256_cmpeq_epi32(r4, a1_broadcast); \
+  r5 = _mm256_cmpeq_epi32(r5, a1_broadcast); \
+  r6 = _mm256_cmpeq_epi32(r6, a1_broadcast); \
+  r7 = _mm256_cmpeq_epi32(r7, a1_broadcast); \
+  flag = (!_mm256_testz_si256(r0, r0)) | \
+          ((!_mm256_testz_si256(r1, r1)) << 1) | \
+          ((!_mm256_testz_si256(r2, r2)) << 2) | \
+          ((!_mm256_testz_si256(r3, r3)) << 3) | \
+          ((!_mm256_testz_si256(r4, r4)) << 4) | \
+          ((!_mm256_testz_si256(r5, r5)) << 5) | \
+          ((!_mm256_testz_si256(r6, r6)) << 6) | \
+          ((!_mm256_testz_si256(r7, r7)) << 7);
 
-#define kernel4(A_0_columns, a1_broadcast, r0, r1, r2, r3, j, flag)            \
-  kernel2(A_0_columns, a1_broadcast, r0, r1, j, flag)                          \
-      kernel2(A_0_columns, a1_broadcast, r2, r3, j, flag)
+#define kernel15(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, \
+                   r8, r9, r10, r11, r12, r13, r14, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r2 = _mm256_loadu_si256(&A_0_columns[j+16]); \
+  r3 = _mm256_loadu_si256(&A_0_columns[j+24]); \
+  r4 = _mm256_loadu_si256(&A_0_columns[j+32]); \
+  r5 = _mm256_loadu_si256(&A_0_columns[j+40]); \
+  r6 = _mm256_loadu_si256(&A_0_columns[j+48]); \
+  r7 = _mm256_loadu_si256(&A_0_columns[j+56]); \
+  r8 = _mm256_loadu_si256(&A_0_columns[j+64]); \
+  r9 = _mm256_loadu_si256(&A_0_columns[j+72]); \
+  r10 = _mm256_loadu_si256(&A_0_columns[j+80]); \
+  r11 = _mm256_loadu_si256(&A_0_columns[j+88]); \
+  r12 = _mm256_loadu_si256(&A_0_columns[j+96]); \
+  r13 = _mm256_loadu_si256(&A_0_columns[j+104]); \
+  r14 = _mm256_loadu_si256(&A_0_columns[j+112]); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast); \
+  r2 = _mm256_cmpeq_epi32(r2, a1_broadcast); \
+  r3 = _mm256_cmpeq_epi32(r3, a1_broadcast); \
+  r4 = _mm256_cmpeq_epi32(r4, a1_broadcast); \
+  r5 = _mm256_cmpeq_epi32(r5, a1_broadcast); \
+  r6 = _mm256_cmpeq_epi32(r6, a1_broadcast); \
+  r7 = _mm256_cmpeq_epi32(r7, a1_broadcast); \
+  r8 = _mm256_cmpeq_epi32(r8, a1_broadcast); \
+  r9 = _mm256_cmpeq_epi32(r9, a1_broadcast); \
+  r10 = _mm256_cmpeq_epi32(r10, a1_broadcast); \
+  r11 = _mm256_cmpeq_epi32(r11, a1_broadcast); \
+  r12 = _mm256_cmpeq_epi32(r12, a1_broadcast); \
+  r13 = _mm256_cmpeq_epi32(r13, a1_broadcast); \
+  r14 = _mm256_cmpeq_epi32(r14, a1_broadcast); \
+  flag = (!_mm256_testz_si256(r0, r0)) | \
+          ((!_mm256_testz_si256(r1, r1)) << 1) | \
+          ((!_mm256_testz_si256(r2, r2)) << 2) | \
+          ((!_mm256_testz_si256(r3, r3)) << 3) | \
+          ((!_mm256_testz_si256(r4, r4)) << 4) | \
+          ((!_mm256_testz_si256(r5, r5)) << 5) | \
+          ((!_mm256_testz_si256(r6, r6)) << 6) | \
+          ((!_mm256_testz_si256(r7, r7)) << 7) | \
+          ((!_mm256_testz_si256(r8, r8)) << 8) | \
+          ((!_mm256_testz_si256(r9, r9)) << 9) | \
+          ((!_mm256_testz_si256(r10, r10)) << 10) | \
+          ((!_mm256_testz_si256(r11, r11)) << 11) | \
+          ((!_mm256_testz_si256(r12, r12)) << 12) | \
+          ((!_mm256_testz_si256(r13, r13)) << 13) | \
+          ((!_mm256_testz_si256(r14, r14)) << 14)
+  
 
-#define kernel8(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j,  \
-                flag)                                                          \
-  kernel4(A_0_columns, a1_broadcast, r0, r1, r2, r3, j, flag)                  \
-      kernel4(A_0_columns, a1_broadcast, r4, r5, r6, r7, j, flag)
+#define kernel4_dual(A_0_columns, a1_broadcast0, a1_broadcast1, r0, r1, r2, r3, r0_a0, r1_a0, r2_a0, r3_a0, j, flag) \
+  r0 = _mm256_loadu_si256(&A_0_columns[j]); \
+  r1 = _mm256_loadu_si256(&A_0_columns[j+8]); \
+  r2 = _mm256_loadu_si256(&A_0_columns[j+16]); \
+  r3 = _mm256_loadu_si256(&A_0_columns[j+24]); \
+  r0_a0 = _mm256_cmpeq_epi32(r0, a1_broadcast0); \
+  r1_a0 = _mm256_cmpeq_epi32(r1, a1_broadcast0); \
+  r2_a0 = _mm256_cmpeq_epi32(r2, a1_broadcast0); \
+  r3_a0 = _mm256_cmpeq_epi32(r3, a1_broadcast0); \
+  r0 = _mm256_cmpeq_epi32(r0, a1_broadcast1); \
+  r1 = _mm256_cmpeq_epi32(r1, a1_broadcast1); \
+  r2 = _mm256_cmpeq_epi32(r2, a1_broadcast1); \
+  r3 = _mm256_cmpeq_epi32(r3, a1_broadcast1); \
+  flag = (!_mm256_testz_si256(r0_a0, r0_a0)) | \
+          ((!_mm256_testz_si256(r1_a0, r1_a0)) << 1) | \
+          ((!_mm256_testz_si256(r2_a0, r2_a0)) << 2) | \
+          ((!_mm256_testz_si256(r3_a0, r3_a0)) << 3) | \
+          ((!_mm256_testz_si256(r0, r0)) << 4) | \
+          ((!_mm256_testz_si256(r1, r1)) << 5) | \
+          ((!_mm256_testz_si256(r2, r2)) << 6) | \
+          ((!_mm256_testz_si256(r3, r3)) << 7);
 
-#define kernel15(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7,    \
-                 r8, r9, r10, r11, r12, r13, r14, j, flag)                     \
-  kernel8(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j, flag)  \
-      kernel4(A_0_columns, a1_broadcast, r8, r9, r10, r11, j, flag)            \
-          kernel2(A_0_columns, a1_broadcast, r12, r13, j, flag)                \
-              kernel1(A_0_columns, a1_broadcast, r14, j, flag)
-
-int sum_up(int counter[], int size) {
-  __m256i r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15;
-
-  int result[8];
-  int total_sum = 0;
-
-  int i = 0;
-  while (i < size) {
-    if (i + 128 <= size) {
-      r0 = _mm256_loadu_si256(&counter[i]);
-      r1 = _mm256_loadu_si256(&counter[i + 8]);
-      r2 = _mm256_loadu_si256(&counter[i + 16]);
-      r3 = _mm256_loadu_si256(&counter[i + 24]);
-      r4 = _mm256_loadu_si256(&counter[i + 32]);
-      r5 = _mm256_loadu_si256(&counter[i + 40]);
-      r6 = _mm256_loadu_si256(&counter[i + 48]);
-      r7 = _mm256_loadu_si256(&counter[i + 56]);
-      r8 = _mm256_loadu_si256(&counter[i + 64]);
-      r9 = _mm256_loadu_si256(&counter[i + 72]);
-      r10 = _mm256_loadu_si256(&counter[i + 80]);
-      r11 = _mm256_loadu_si256(&counter[i + 88]);
-      r12 = _mm256_loadu_si256(&counter[i + 96]);
-      r13 = _mm256_loadu_si256(&counter[i + 104]);
-      r14 = _mm256_loadu_si256(&counter[i + 112]);
-      r15 = _mm256_loadu_si256(&counter[i + 120]);
-      kernel_add16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13,
-                   r14, r15);
-      i += 128;
-    } else if (i + 64 <= size) {
-      r0 = _mm256_loadu_si256(&counter[i]);
-      r1 = _mm256_loadu_si256(&counter[i + 8]);
-      r2 = _mm256_loadu_si256(&counter[i + 16]);
-      r3 = _mm256_loadu_si256(&counter[i + 24]);
-      r4 = _mm256_loadu_si256(&counter[i + 32]);
-      r5 = _mm256_loadu_si256(&counter[i + 40]);
-      r6 = _mm256_loadu_si256(&counter[i + 48]);
-      r7 = _mm256_loadu_si256(&counter[i + 56]);
-      kernel_add8(r0, r1, r2, r3, r4, r5, r6, r7);
-      i += 64;
-    } else if (i + 32 <= size) {
-      r0 = _mm256_loadu_si256(&counter[i]);
-      r1 = _mm256_loadu_si256(&counter[i + 8]);
-      r2 = _mm256_loadu_si256(&counter[i + 16]);
-      r3 = _mm256_loadu_si256(&counter[i + 24]);
-      kernel_add4(r0, r1, r2, r3);
-      i += 32;
-    } else if (i + 16 <= size) {
-      r0 = _mm256_loadu_si256(&counter[i]);
-      r1 = _mm256_loadu_si256(&counter[i + 8]);
-      kernel_add2(r0, r1);
-      i += 16;
-    } else {
-      r0 = _mm256_loadu_si256(&counter[i]);
-      i += 8;
-    }
-    _mm256_storeu_si256(result, r0);
-    for (int i = 0; i < 8; i++) {
-      total_sum += result[i];
-    }
-  }
-  return total_sum;
-}
 
 void matrix_multiply_simd(const int A_0_columns[], const int A_0_row_ptr[],
                           int a_1_columns_start, int num_rows_A_0,
@@ -144,7 +183,6 @@ void matrix_multiply_simd(const int A_0_columns[], const int A_0_row_ptr[],
     for (int col_idx = a_1_columns_start;
          col_idx < a_1_columns_start + num_cols_a_1; col_idx++) {
 
-      st = rdtsc();
       a1_broadcast = _mm256_set1_epi32(A_0_columns[col_idx]);
 
       int j = A_0_row_ptr[i]; // A0_start
@@ -153,60 +191,27 @@ void matrix_multiply_simd(const int A_0_columns[], const int A_0_row_ptr[],
 
       while (j < A0_end - 8) {
         if (j + 120 < A0_end) {
-          r0 = _mm256_loadu_si256(&A_0_columns[j]);
-          r1 = _mm256_loadu_si256(&A_0_columns[j + 8]);
-          r2 = _mm256_loadu_si256(&A_0_columns[j + 16]);
-          r3 = _mm256_loadu_si256(&A_0_columns[j + 24]);
-          r4 = _mm256_loadu_si256(&A_0_columns[j + 32]);
-          r5 = _mm256_loadu_si256(&A_0_columns[j + 40]);
-          r6 = _mm256_loadu_si256(&A_0_columns[j + 48]);
-          r7 = _mm256_loadu_si256(&A_0_columns[j + 56]);
-          r8 = _mm256_loadu_si256(&A_0_columns[j + 64]);
-          r9 = _mm256_loadu_si256(&A_0_columns[j + 72]);
-          r10 = _mm256_loadu_si256(&A_0_columns[j + 80]);
-          r11 = _mm256_loadu_si256(&A_0_columns[j + 88]);
-          r12 = _mm256_loadu_si256(&A_0_columns[j + 96]);
-          r13 = _mm256_loadu_si256(&A_0_columns[j + 104]);
-          r14 = _mm256_loadu_si256(&A_0_columns[j + 112]);
           kernel15(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7,
                    r8, r9, r10, r11, r12, r13, r14, j, flag);
           j += 120;
         } else if (j + 64 < A0_end) {
-          r0 = _mm256_loadu_si256(&A_0_columns[j]);
-          r1 = _mm256_loadu_si256(&A_0_columns[j + 8]);
-          r2 = _mm256_loadu_si256(&A_0_columns[j + 16]);
-          r3 = _mm256_loadu_si256(&A_0_columns[j + 24]);
-          r4 = _mm256_loadu_si256(&A_0_columns[j + 32]);
-          r5 = _mm256_loadu_si256(&A_0_columns[j + 40]);
-          r6 = _mm256_loadu_si256(&A_0_columns[j + 48]);
-          r7 = _mm256_loadu_si256(&A_0_columns[j + 56]);
-          kernel8(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j,
-                  flag);
+          kernel8(A_0_columns, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j, flag);
           j += 64;
         } else if (j + 32 < A0_end) {
-          r0 = _mm256_loadu_si256(&A_0_columns[j]);
-          r1 = _mm256_loadu_si256(&A_0_columns[j + 8]);
-          r2 = _mm256_loadu_si256(&A_0_columns[j + 16]);
-          r3 = _mm256_loadu_si256(&A_0_columns[j + 24]);
-          kernel4(A_0_columns, a1_broadcast, r0, r1, r2, r3, j, flag);
+          kernel4_dual(A_0_columns, a1_broadcast, a1_broadcast, r0, r1, r2, r3, r4, r5, r6, r7, j, flag);
           j += 32;
         } else if (j + 16 < A0_end) {
-          r0 = _mm256_loadu_si256(&A_0_columns[j]);
-          r1 = _mm256_loadu_si256(&A_0_columns[j + 8]);
           kernel2(A_0_columns, a1_broadcast, r0, r1, j, flag);
           j += 16;
         } else {
-          r0 = _mm256_loadu_si256(&A_0_columns[j]);
           kernel1(A_0_columns, a1_broadcast, r0, j, flag);
           j += 8;
         }
-        if (flag != 0) {
+        if (flag) {
           counter++;
           break;
         }
       }
-      et = rdtsc();
-      sum += (et - st);
     }
     
     butterfly_count += counter * counter - counter;
@@ -314,6 +319,7 @@ int main(int argc, char **argv) {
   // IA: row_ptr, JA: col_idx
   uint64_t A_0_columns_origin[MAX_EDGES], A_0_row_ptr_origin[MAX_EDGES];
   uint64_t node_count = read_edge_list_CSR("/afs/andrew.cmu.edu/usr10/xinyuc2/private/18645/project/butterfly/data/opsahl-collaboration/out.opsahl-collaboration", A_0_row_ptr_origin, A_0_columns_origin);
+  // uint64_t node_count = read_edge_list_CSR("/afs/andrew.cmu.edu/usr10/xinyuc2/private/18645/project/butterfly/data/test/input.txt", A_0_row_ptr_origin, A_0_columns_origin);
   int num_rows_A_0 = 16726;
   int num_cols_A_0 = node_count;
   printf("Node count = %d\n", node_count);
@@ -345,8 +351,11 @@ int main(int argc, char **argv) {
       printf("row: %d\n", a_1_row);
       
       // SIMD test
+      st = rdtsc();
       matrix_multiply_simd(A_0_columns, A_0_row_ptr, a_1_columns_start, a_1_row,
                            num_cols_A_0, num_cols_a_1);
+      et = rdtsc();
+      sum += (et - st);
       
       // // Scalar test
       // st = rdtsc();
@@ -362,7 +371,7 @@ int main(int argc, char **argv) {
   }
 
   // num_ops = 2162;  // needed for SIMD
-  num_ops=6344270925;
+  num_ops=4673066757;
   // num_ops /= runs;   // needed for scalar
   printf("num_ops=%llu\n", num_ops);
   printf("RDTSC Base Cycles Taken: %llu\n\r", sum);
